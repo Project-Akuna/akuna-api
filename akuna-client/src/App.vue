18faa391cdde6530c52f5740d1c6d99d9f967ce8
Reymark Arsenio <jarvs.tensorbros@gmail.com>
2019-12-10 15:38:54 +0800

    Vue initial commit

---
+++ akuna-client/src/App.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  v-app
+    //- navbar
+    v-content
+      router-view
+</template>
+
+<script>
+import Navbar from './components/navigation/navbar'
+export default {
+  name: 'App',
+
+  components: {
+    Navbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style lang="scss">
+.v-text-field {
+  .v-input__prepend-inner i {
+    padding-left: 10px;
+    transform: scale(0.8);
+  }
+  .v-input__append-inner i {
+    transform: scale(0.8);
+    padding-right: 10px;
+  }
+  input {
+    font-size: 14px;
+    margin-left: 10px;
+  }
+  label {
+    margin-left: 10px;
+    font-size: 14px;
+  }
+  
+}
+</style>
